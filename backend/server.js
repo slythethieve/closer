4c08908a86f84022a337410263cff67adee7d0d2
Matteo De Sabato <desabatomatteo@yahoo.it>
2022-08-16 16:33:57 +0200

    First get request working

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT
+
+const app = express()
+
+
+app.get('/api/orders', (req, res) => {
+    res.json({
+        message: "Get orders"
+    })
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
