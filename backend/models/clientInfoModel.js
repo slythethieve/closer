32a421356075eeb54c4d5b71da0bcdcde9d53d9e
Matteo De Sabato <desabatomatteo@yahoo.it>
2022-08-17 21:54:53 +0200

    Small addition to schemas

--- backend/models/clientInfoModel.js
+++ backend/models/clientInfoModel.js
@@ -8,5 +8,26 @@ const clientInfoSchema = mongoose.Schema({
     lastName: {
         type: String,
         required: [true, 'Please enter a last name']
+    },
+    homeAdressStreet: {
+        type: String,
+        required: [true, 'Please enter an adress']
+    },
+    homeAdressNumber: {
+        type: Number,
+        required: [true, 'Please enter a the house number']
+    },
+    PLZ: {
+        type: Number
+    },
+    city: {
+        type: String,
+        required: [true, 'Please enter the client city']
+    },
+    phoneNumber: {
+        type: String,
+    },
+    email: {
+        type: String
     }
 })
