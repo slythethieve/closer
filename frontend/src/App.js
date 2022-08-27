46a063b84a5b43032d91ea92f358a976bf931d20
Matteo De Sabato <desabatomatteo@yahoo.it>
2022-08-27 17:41:33 +0200

    Started with UI and styles: sidebar in particular

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Dashboard from './Pages/Dashboard';
+import Dashboard from './Pages/dashboard/Dashboard';
 import Login from './Pages/Login';
+
+
 
 function App() {
   return (
     <>
       <Router>
-        <div className='container'>
+        <div>
           <Routes>
             <Route path='/' element = {<Dashboard />} />
             <Route path='/login' element = {<Login />} />  
---
+++ frontend/src/Pages/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import Sidebar from "../../components/navbar/sidebar/Sidebar"
+import "./dashboard.scss"
+
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <Sidebar />
+      <div className="homeContainer">container</div>
+    </div>
+  )
+}
+
+export default Dashboard
