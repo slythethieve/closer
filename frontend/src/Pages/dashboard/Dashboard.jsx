06846674d07bfc09197f258a8474e193e65a176b
Matteo De Sabato <desabatomatteo@yahoo.it>
2022-10-04 18:43:29 +0200

    found a suitable way to caclulate dates and do comparisons with the dates in db

--- frontend/src/Pages/dashboard/Dashboard.jsx
+++ frontend/src/Pages/dashboard/Dashboard.jsx
@@ -5,12 +5,18 @@ import FeaturedChart from "../../components/featuredChart/FeaturedChart"
 import Chart from "../../components/chart/Chart"
 import TableComponent from "../../components/table/TableComponent"
 import "./dashboard.scss"
-import axios from 'axios'
 
 // Ok so let's think about this for a hot minute. 
 // I need a bunch of functions for fetching data from the db and show it 
 // in my dashboard. 
 
+// Let's also do a recap: 
+// - I can probably now sum up the number of quotes of one company and the other
+// - I can probably quickly implement how many contracts there are (just check for the flag)
+// - I need to tinker a bit to get the total price working again (again need to check for isOrder flag)
+// - The last 2 functions which require a bit more thought are the ones using dates. 
+// - In fact even tho I can implement the first 3 functions I need to implement the date filtering first
+//   Also especially for the graph widgets I should really use the filter method instead of loops to ease the calculations. 
 import { useSelector, useDispatch } from 'react-redux';
 import { getQuotes } from '../../features/quote/quoteSlice';
 import { useEffect, useState } from 'react'
@@ -21,55 +27,54 @@ import { useEffect, useState } from 'react'
 
 function Dashboard() {
 
+  // Maybe this is the best way to do solve this. 
+  // I can probably put some parameters in this functions (maybe change it to const),
+  // add some parameters for the start end date and finally parse the values to do 
+  // comparisons with the mongo db date format. 
+  function getFirstDayPreviousMonth() {
+    const date = new Date();
+    return new Date(date.getFullYear(), date.getMonth() - 1, 1);
+  }
 
-  
-  
-  // Date stuff
-  
-  let date = new Date()
-  
-  console.log("Before " + Date.parse(date))
-  let year = date.getFullYear()
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-  // + 1 because it starts at 0. That is dumb for sure. 
-  let currentMonth = date.getMonth() + 1,
-      previousMonth_1 = date.getMonth()
-  // OK OK so with this I can get to a month before my current date. 
-  // How can I get to the start of the month tho?
-  let testsuperDate = date.setMonth(date.getMonth()-1)
-  console.log("after " + testsuperDate)
-  
+  //console.log("Man " +getFirstDayPreviousMonth());
 
-  
+  // Ok now let's try and get the last day of the previous month
+  // Ok I did it as well. 
+  function getLastDayPreviousMonth() {
+    const date = new Date()
+    return new Date(date.getFullYear(), date.getMonth(), 0)
+  }
 
 
-  
+
+
 
   const dispatch = useDispatch()
 
   const [goodlineeEntries, setGoodlineeEntries] = useState([])
 
+  // Change this names to better ones.
   const [total, setTotal] = useState(0)
   const [numberOfClients, setNumberOfClients] = useState(0)
 
-  const {quote, isLoading, isError, message} = useSelector((state) => 
-        state.quotes)
+  const { quote, isLoading, isError, message } = useSelector((state) =>
+    state.quotes)
 
   useEffect(() => {
 
-    if(isError) {
-      console.log('message')
+    if (isError) {
+      console.log(message)
     }
-    
+
 
     dispatch(getQuotes())
 
-    
+
     calculateTotal(quote)
     setNumberOfClients(quote.length)
     //test(quote)
     testDate(quote)
-    
+
   }, [quote.length, isError, message, dispatch])
 
 
@@ -78,66 +83,67 @@ function Dashboard() {
     for (let i = 0; i < quote.length; i++) {
       //console.log(quote[i].date)
       //console.log(Date.parse(quote[i].date))
-      if (Date.parse(quote[i].date) < date) {
-        //console.log(quote[i])
-      } 
+      if (Date.parse(quote[i].date) > Date.parse(getFirstDayPreviousMonth())) {
+        console.log(quote[i])
+      }
     }
-   }
+  }
   // I want to try and calculate the number of offers that belong to one company or the other
 
-  // So I think I made it work
+  // So I think I made it work. just need to change the length. if one offer has 3 products
+  // it does not count as 3 offers but just one. D'uh. 
   const test = (quote) => {
-    
+
 
     let k = 0
     for (let i = 0; i < quote.length; i++) {
       try {
-        for (let j= 0; j< Object.values(quote[i].products.euroMoebelProducts.products).length; j++) {
-        
-        
-        //console.log(Object.values(quote[i].products.goodlineeProducts.products))
-        //console.log(k)
-        k++
-        
+        for (let j = 0; j < Object.values(quote[i].products.euroMoebelProducts.products).length; j++) {
+
+
+          //console.log(Object.values(quote[i].products.goodlineeProducts.products))
+          //console.log(k)
+          k++
+
         }
       } catch (error) {
-        
+
       }
-      
+
     }
 
     return k;
-    
-    
+
+
   }
 
   // Total revenue. With the new db schema I also need to change this one. 
   const calculateTotal = (quote) => {
     let totalRev = 0
     for (let i = 0; i < quote.length; i++) {
-      for (let j= 0; j< Object.values(quote[i].products).length; j++) {
-        
+      for (let j = 0; j < Object.values(quote[i].products).length; j++) {
+
         totalRev = totalRev + (Number(Object.values(quote[i].products)[j].price) || 0)
-        
+
       }
     }
     setTotal(totalRev)
   }
 
-  
+
   return (
     <div className="dashboard">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          <Widget type="invoices" total={test(quote)}/>
-          <Widget type="contracts" total = {numberOfClients}/>
-          <Widget type="revenue" total ={total}/>
-          <Widget type="clients" total ={numberOfClients}/>
+          <Widget type="invoices" total={test(quote)} />
+          <Widget type="contracts" total={numberOfClients} />
+          <Widget type="revenue" total={total} />
+          <Widget type="clients" total={numberOfClients} />
         </div>
         <div className="charts">
-          <FeaturedChart total= {total}/>
+          <FeaturedChart total={total} />
           <Chart />
         </div>
         <div className="listContainer">
@@ -145,7 +151,7 @@ function Dashboard() {
         </div>
       </div>
     </div>
-    
+
   )
 }
 
